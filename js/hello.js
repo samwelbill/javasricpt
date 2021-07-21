2476739ef8c7427dfe11529dc7da63ca8bc01083
samwelbill <orerobill@gmail.com>
2021-07-21 11:19:22 +0300

    Linking js file in html

---
+++ js/hello.js
@@ -0,0 +1,10 @@
+// A function to display a message
+function sayHello() {
+    
+    var greet = 'Welcome to school';
+    alert (greet);
+
+}
+
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
